3e4d028812f0163e32d0aea8e0a4554f325e1d01
pollyp0 <9033303010@mail.ru>
2024-07-10 18:16:29 +0300

    eslintrs change

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -28,11 +28,11 @@ export default function About() {
                     <div id="hello" className={rubikMonoOne.className}>
                         ABOUT ME
                     </div>
-                    <span>I'm Polina Pokhodyaeva, </span><span className="blue-text">front-end developer</span> and
+                    <span>I am Polina Pokhodyaeva, </span><span className="blue-text">front-end developer</span> and
                     <span className="blue-text"> UI/UX designer </span><span>with one year of experience.
         Now I am studying at </span><span className="blue-text">Innopolis University</span>,
                     enjoying student life and continuing to hone my skills.
-                    When I'm not coding, you can most likely find me at the gym, sketching, playing guitar in the
+                    When I am not coding, you can most likely find me at the gym, sketching, playing guitar in the
                     rehearsal
                     room, or singing. <span className="blue-text">Welcome to my corner of the web!</span>
                 </div>
